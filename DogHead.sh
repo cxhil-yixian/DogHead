6e5fd76ae5fbcdc7fa3b874f851d1fd5ed694c61
cxhil-yixian <cxhil.yixian@gmail.com>
2025-08-21 11:43:17 +0800

    Renew 0821

--- DogHead.sh
+++ DogHead.sh
@@ -5,7 +5,7 @@
 README_URL="https://raw.githubusercontent.com/cxhil-yixian/DogHead/main/README.md"
 
 # 下載並輸出 README.md
-curl -sSL "$README_URL" | less
+curl -sSL "$README_URL"
 
 # 提示使用者按任意鍵繼續
 echo "按任意鍵繼續..."
