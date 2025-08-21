851493cf5cfd99deedc88bb97537f44d637cc30f
cxhil-yixian <cxhil.yixian@gmail.com>
2025-08-21 11:41:55 +0800

    Renew 0821

---
+++ DogHead.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# DogHead.sh
+
+# GitHub 原始檔 URL
+README_URL="https://raw.githubusercontent.com/cxhil-yixian/DogHead/main/README.md"
+
+# 下載並輸出 README.md
+curl -sSL "$README_URL" | less
+
+# 提示使用者按任意鍵繼續
+echo "按任意鍵繼續..."
+read -n 1 -s
